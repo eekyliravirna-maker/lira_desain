5180c799ea9dd699026b001d536ed557eadee47f
eekyliravirna-maker <eekyliravirna@gmail.com>
2025-10-16 09:15:45 +0700

    Add files via upload

---
+++ scripts/objRefTable.js
@@ -0,0 +1,33 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Plugins.Touch,
+		C3.Plugins.Text,
+		C3.Plugins.Touch.Cnds.OnTapGestureObject,
+		C3.Plugins.System.Acts.GoToLayout
+	];
+};
+self.C3_JsPropNameTable = [
+	{Sprite: 0},
+	{Sprite2: 0},
+	{Touch: 0},
+	{Sprite3: 0},
+	{Text: 0},
+	{Sprite4: 0},
+	{Sprite5: 0},
+	{Sprite6: 0},
+	{Text2: 0}
+];
+
+self.InstanceType = {
+	Sprite: class extends self.ISpriteInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	Touch: class extends self.IInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
+	Text: class extends self.ITextInstance {},
+	Sprite4: class extends self.ISpriteInstance {},
+	Sprite5: class extends self.ISpriteInstance {},
+	Sprite6: class extends self.ISpriteInstance {},
+	Text2: class extends self.ITextInstance {}
+}
